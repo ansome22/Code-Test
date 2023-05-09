4122cd119770831ff06168e7072051feedc803b8
Alex Thomas <ansome22@gmail.com>
2023-05-09 12:56:16 +1200

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# Code Challenge Front end back end
+
+## Introduction
+
+
+## Setup
+
+# windows machine
+venv\Scripts\activate.bat
+
+#mac/linux
+source venv/bin/activate
---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [{ path: 'core', loadChildren: () => import('./core/core.module').then(m => m.CoreModule) }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }

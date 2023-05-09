508f182c627c995c00ea4b604b5812ab5632eba0
Alex Thomas <ansome22@gmail.com>
2023-05-09 17:08:44 +1200

    prepare mock data (note: will continue tomorrow)

---
+++ frontend/src/app/shared/services/data/mock/product.mock.ts
@@ -0,0 +1,11 @@
+import { Product } from "src/app/shared/models/product";
+
+export const PRODUCTS: Product[] = [
+{ sku: "aasr", name: 'Door', description: "aaa", price: 10 },
+{ sku: "aasr", name: 'Cat', description: "aaa", price: 100 },
+{ sku: "aasr", name: 'Mice', description: "aaa", price: 30 },
+{ sku: "aasr", name: 'Chair', description: "aaa", price: 10 },
+{ sku: "aasr", name: 'Bed', description: "aaa", price: 400 },
+{ sku: "aasr", name: 'Lamp', description: "aaa", price: 20 },
+{ sku: "aasr", name: 'Table', description: "aaa", price: 100 },
+];
